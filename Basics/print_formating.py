7c7913c08a643de5470ab312b01ec25b2b5c754f
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-04 12:08:43 +0530

    print_formating.py

---
+++ Basics/print_formating.py
@@ -0,0 +1,9 @@
+# print() formating
+# Author: Arshika
+# coverd print() formating 
+# print('   ')
+# peint("    ")
+# print('''    ''')
+print('hii, i am Arshika')
+print("hii, I'm Arshika")
+print('''hii, i'm Arshika ''')
