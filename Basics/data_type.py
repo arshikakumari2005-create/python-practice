ad85ed12c5e258ef604639e9aa8c2beb911b07e1
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-04 09:38:53 +0530

    data_type.py

---
+++ Basics/data_type.py
@@ -0,0 +1,17 @@
+# Python Data type
+# Author=Arshika kumari
+# learning Python
+# Topic coverd (string,Integer,float,boolean)
+# type() function
+name="Arshika Kumari"
+age=21
+roll_no=8
+marks=85.80
+is_pass=True
+message="My name is Arshika, i am 21 years old"
+print(type(name),name)
+print(type(age),age)
+print(type(roll_no),roll_no)
+print(type(marks),marks)
+print(type(is_pass),is_pass)
+print(type(message),message)
