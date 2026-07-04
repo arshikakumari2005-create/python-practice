318e87cbb11eade47aad2ae13edef04f157b6110
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-04 22:23:38 +0530

    Arithmetic.py

---
+++ Operators_In_Python/Arithmetic.py
@@ -0,0 +1,14 @@
+# operators in python
+# Arithmetic operators
+
+num = 24
+num2= 15
+print(int(num2) + float(num))
+print(num + num2)
+print(num - num2 )
+print(num * num2)
+print(num ** 2)
+print(num / num2)
+print(num // num2)
+print(num % 5)
+
