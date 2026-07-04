c27e406323cefb6cf5a2848fd7bfdc28adfa97bf
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-04 23:14:01 +0530

    String_Indexing.py

---
+++ String/String_Indexing.py
@@ -0,0 +1,14 @@
+# String indexting
+# index start with 0 
+# length start with 1
+name="Arshika"
+# index = Arshika---0123456
+# length= Arshika---1234567
+print(len(name))
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
