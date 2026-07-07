122fdf57ca5ed4f79f17037f171dde04bbbf4ca7
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-07 18:40:45 +0530

    string_comparison.py

---
+++ String/string_comparison.py
@@ -0,0 +1,18 @@
+# string comparison in python
+
+print("apple" == "Apple")
+# False
+print("apple" == "apple")
+# True
+print("A" > "Z")
+# False
+print("Z" > "A")
+# True 
+print("a" < "b")
+# True
+print("a" > "b")
+# False
+print("cat" > "bat")
+# True
+print("bat" > "cat")
+# False
