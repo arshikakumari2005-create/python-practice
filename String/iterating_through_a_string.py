09ba938349be39e4d20503ac3da6793dcd375b27
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-07 18:54:36 +0530

    iterating_through_a_string.py

---
+++ String/iterating_through_a_string.py
@@ -0,0 +1,23 @@
+# iterating_through_a_string
+
+for chara in "Python":
+    print(chara)
+
+# o\p 
+# P
+# y
+# t
+# h
+# o
+# n
+
+for ch in "Arshika":
+    print(ch)
+
+# A
+# r
+# s
+# h
+# i
+# k
+# a    
