190b0bc5e12627a690d6c006a2360fa688e96786
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-07 19:20:29 +0530

    imp_built_in_function.py

---
+++ String/imp_built_in_function.py
@@ -0,0 +1,18 @@
+# important built in function 
+# len(), max(), min(),sorted()
+name="arshika"
+print(len(name))
+print(max(name))
+print(min(name))
+print(sorted(name)) 
+
+num="2468"
+print(len(num))
+print(max(num))
+print(min(num))
+print(sorted(num))
+
+
+
+ 
+
