889ef0382c7c74254ea0dbf930e6895c5e54c589
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-09 22:55:05 +0530

    list_intro.py

---
+++ list/list_intro.py
@@ -0,0 +1,18 @@
+# list intro
+# In Python, a list is a built-in data structure used to store an ordered, mutable collection of items.
+# Written inside square brackets 👉🏻[] with elements separated by commas,
+# lists can hold duplicate items and completely mixed data types simultaneously
+#📝 A standard Python list containing an integer, string, float, and boolean
+
+fruits=["apple","banana","orange"]
+num=[1,2,3,4,5,6,7]
+float_num=[2.5,3.5,6.11]
+store_bool=[True,False]
+all_mix=["apple",4,True,6,"arshika",3.5]
+
+# then 👇 display
+print(all_mix)
+print(num)
+print(float_num)
+print(store_bool)
+print(fruits)
