6fe1ffe1e22b6173faf7cb338ad28fc3af2982d8
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-10 16:34:31 +0530

    list_slicing.py

---
+++ list/list_slicing.py
@@ -0,0 +1,16 @@
+# list Slicing
+num=[51,52,53,54,55]
+print(len(num))
+print(num[0:5])
+print(num[0:])
+print(num[0:5])
+print(num[:5])
+print(num[1:5])
+print(num[2:5])
+print(num[3:5])
+print(num[4:6])
+print(num[-6:-1])
+print(num[-1::-1])
+print(num[::-1])
+print(num[::-2])
+
