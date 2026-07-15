080bf4dda614cf6450aa21c575dacc6ec4ceef48
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-15 20:50:13 +0530

    item_change.py

---
+++ list/item_change.py
@@ -0,0 +1,8 @@
+# Item change in list 
+my_list=["Arhika","Akanksha","Ravi","Rohan","aayush","Aman","Ravina",]
+print(my_list)
+my_list[-2]="Ram"
+print(my_list)
+my_list[0]="Buchhu"
+print(my_list)
+
