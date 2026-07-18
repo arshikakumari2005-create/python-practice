948aa859d9f258ccaf4b83b8eeb64ebe9e766283
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-18 18:34:39 +0530

    Duplicate_key.py

--- Dectionary/Duplicate_key.py
+++ Dectionary/Duplicate_key.py
@@ -1,2 +1,12 @@
 # Duplicate key
-# In dictionary duplicate Key is not allow.
+#  Duplicate Key are not allowed in python.
+student_name={"name":"Arshika",
+              "name":"Rohan",
+              "name":"Resama"
+              }
+
+print(student_name)
+print(student_name["name"])
+print(len(student_name))
+
+# Now it's ready to run 👍🏻
