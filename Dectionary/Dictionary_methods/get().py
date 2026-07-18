23b0c28111039118e18c9668b4d5fdab75d74acb
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-18 20:30:50 +0530

    git().py

---
+++ Dectionary/Dictionary_methods/get().py
@@ -0,0 +1,11 @@
+# get()
+student={"name":"Arshika",
+         "age":20
+         }
+print(student.get("name"))
+# Arshika
+print(student.get("age"))
+# 20
+print(student.get("roll_no"))
+# None  
+
