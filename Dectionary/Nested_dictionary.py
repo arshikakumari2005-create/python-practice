23bfdaf8c63b9450de8b67473d9d91ebb1ea0407
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-18 20:02:04 +0530

    Nested_dictionary.py

---
+++ Dectionary/Nested_dictionary.py
@@ -0,0 +1,9 @@
+# Nested Dictionary
+state={"jharkhand":{"city":["ranchi","jamshedpur","bokaro"]},
+        "bihar":{"city":["ara","patna"]}            
+        }
+print(state)
+print(state["jharkhand"])
+print(state["jharkhand"]["city"][1])
+print(state["bihar"])
+print(state["bihar"]["city"][0])
