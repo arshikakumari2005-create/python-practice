7da3e1bc7254fe460241ca00712e588bcf285c8e
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-19 12:27:00 +0530

    values().py

---
+++ Dectionary/Dictionary_methods/values().py
@@ -0,0 +1,9 @@
+# The values() method return all the value in the dictionary.
+student={"name":"Arshika",
+         "age":20,
+         "roll_no":22
+         }
+print(student)
+print(f"Roll_no = {student.get("roll_no")}")
+print(f"All keys = {student.keys()}")
+print(f"All values = {student.values()}")
