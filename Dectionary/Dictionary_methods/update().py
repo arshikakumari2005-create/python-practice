0d0b869080c032ab7eb125abf9289809d59f9cae
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-19 13:05:40 +0530

    update().py

---
+++ Dectionary/Dictionary_methods/update().py
@@ -0,0 +1,10 @@
+# update()
+# The update() method adds key-value pairs or update the value of an existing key.
+student={"name":"Arshika",
+         "age":20,
+         }
+print(student)
+student.update({"roll_no":22})
+print(student)
+student.update({"class":"10th"})
+print(student)
