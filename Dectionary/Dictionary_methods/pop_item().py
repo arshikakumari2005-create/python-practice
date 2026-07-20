c89c41d6f68ad787b8bf9f83dfa8d9c73e4f6c18
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-20 09:04:23 +0530

    pop_item().py

---
+++ Dectionary/Dictionary_methods/pop_item().py
@@ -0,0 +1,8 @@
+# popitem()
+# The popitem() method remove and returns the last inserted key-value pair.
+student={"name":"Arshika",
+         "age":20,
+         "roll_no":22}
+print(student)
+print("Last item remove = ",student.popitem())
+print(student)
