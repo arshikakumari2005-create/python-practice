c8e588c68fa84e8fa25c419cefa786fb5e085a8c
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-20 18:59:27 +0530

    fromkeys().py

---
+++ Dectionary/Dictionary_methods/fromkeys().py
@@ -0,0 +1,9 @@
+# fromkeys()
+# creates dictionary from key.
+student={}
+change=student.fromkeys(["a","b","c"],10)
+print(student)
+print(change)
+print(student)
+change2=student.fromkeys(["d","e","f"],20)
+print(change2)
