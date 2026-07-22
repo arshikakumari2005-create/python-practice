3fe94e9c3aaf1375d56b201fd27385d4b538a731
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-22 16:55:38 +0530

    logical_operator.py

---
+++ condition(if_elif_else)/logical-operator.py
@@ -0,0 +1,10 @@
+# logical operator
+#  not 
+#  or 
+#  and 
+age=int(input("enter age = "))
+if age>3 and age>16:
+    print("you are not eligible for ride")
+else:
+    print("you are eligible for ride") 
+      
